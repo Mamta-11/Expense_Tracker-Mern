9dd10fac2f094c7391c5cd66913df8ee7bb3dc50
mamta <mt6100378@gmail.com>
2026-04-21 12:04:03 +0530

    Fixed CORS error for localhost

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://expense-tracker-mern-jade.vercel.app" || "http://localhost:5173", 
+    origin:"*", 
     credentials: true 
 }));
 
